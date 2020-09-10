378c20d2a2696964bad5daf6f0e8b53ca32b3880
Frank <lifrank@grinnell.edu>
2020-09-10 12:49:03 -0500

    add cli to parse

--- parse.py
+++ parse.py
@@ -1,7 +1,6 @@
 import pickle
 import re
 from collections import defaultdict, Counter
-from bs4 import BeautifulSoup
 
 def parse(string, markov, weight):
 
@@ -9,38 +8,34 @@ def parse(string, markov, weight):
     
     words = re.finditer(p, string)
 
-    if (last := next(words, None)) != None:
+    if (last := next(words, None)) is not None:
         last = last.group()
-    while (n := next(words, None)) != None:
+    while (n := next(words, None)) is not None:
         n = n.group()
         markov[last.lower()][n] += weight
         last = n
 
 markov = defaultdict(Counter)
 
-with open('../../Downloads/copperfield.html') as c:
-    soup = BeautifulSoup(c, 'lxml')
+model = None
+model_location = None
 
-    text = soup.findAll('p')
-
-    for t in text:
-        parse(t.text, markov, 1)
-
-with open('../../Downloads/lolita.html') as c:
-    soup = BeautifulSoup(c, 'lxml')
-
-    t = soup.find('pre')
-
-    parse(t.text, markov, 2)
-
-with open('./computer.txt') as c:
-    parse(c.read(), markov, 10)
-
-with open('../../Downloads/my immortal.txt') as c:
-    f = c.read()
-    parse(f, markov, 4)
-
-print(markov)
-
-with open('./markov.bin', 'wb') as c:
-    pickle.dump(markov, c)
+while (cmd := input('Enter l to load (or create) model, a to add new file, q to quit')) != 'q':
+    if cmd == 'l':
+        pass
+    if cmd == 'a':
+        if not model is None:
+            print('load a model file before adding text')
+        else:
+            filepath = input('enter the location of the file to add.')
+            weight = input('enter the weight of the file (1)')
+            if type(weight) is not int:
+                print('please enter a valid integer')
+            else:
+                with open(filepath) as f:
+                    string = f.read()
+                    parse(string, model, weight)
+                
+                pickle.dump(model, open(model_location, 'w'))
+    else:
+        print('invalid command. press "q" to quit')
