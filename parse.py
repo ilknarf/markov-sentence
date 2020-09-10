1b2a0d7338248a9c4ea788377f4fce880103a0af
Frank <lifrank@grinnell.edu>
2020-09-10 12:58:35 -0500

    finish adding parser commands

--- parse.py
+++ parse.py
@@ -1,3 +1,5 @@
+import os
+
 import pickle
 import re
 from collections import defaultdict, Counter
@@ -15,20 +17,33 @@ def parse(string, markov, weight):
         markov[last.lower()][n] += weight
         last = n
 
-markov = defaultdict(Counter)
-
 model = None
 model_location = None
 
-while (cmd := input('Enter l to load (or create) model, a to add new file, q to quit')) != 'q':
+while (cmd := input('Enter l to load (or create) model, a to add new file, q to quit: ')) != 'q':
     if cmd == 'l':
-        pass
+        if model is not None:
+            print('model is already loaded')
+        else:
+            model_location = input('enter the location (or desired location_ of the model: ')
+            if os.path.exists(model_location):
+                if os.path.isdir(model_location):
+                    print('model location is directory')
+                else:
+                    try:
+                        with open(model_location, 'w') as file:
+                            model = pickle.load(file)
+                    except:
+                        print('invalid file contents')
+            else:
+                model = defaultdict(Counter)
+
     if cmd == 'a':
-        if not model is None:
+        if model is None:
             print('load a model file before adding text')
         else:
-            filepath = input('enter the location of the file to add.')
-            weight = input('enter the weight of the file (1)')
+            filepath = input('enter the location of the file to add: ')
+            weight = input('enter the weight of the file (1): ')
             if type(weight) is not int:
                 print('please enter a valid integer')
             else:
